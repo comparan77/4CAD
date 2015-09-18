f6a41951c97fb6fe084b255f2dc16d2c59247991
Gil win7 <comparan77@gmail.com>
2015-09-17 21:33:02 -0500

    Default en fechas

--- AppCasc/js/report/frmReportViewer.js
+++ AppCasc/js/report/frmReportViewer.js
@@ -5,10 +5,12 @@
     }
 
     function initControls() {
+        var currentDate = new Date();
+        currentDate = moment(currentDate).format('DD/MM/YYYY');
         $('#ctl00_body_btnGetRpt').button();
         $('#ctl00_body_txt_fecha_ini, #ctl00_body_txt_fecha_fin').datepicker({
             'dateFormat': 'dd/mm/yy'
-        })
+        }).val(currentDate);
     }
 }
 
