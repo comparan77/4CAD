5b73b5336947593611323d4aedec6580ddbc0746
comparan77 <comparan77@gmail.com>
2018-02-13 19:46:18 -0600

    ready

--- AppCasc/js/operation/frmOrdenTrabajo.js
+++ AppCasc/js/operation/frmOrdenTrabajo.js
@@ -2,6 +2,8 @@
 
     this.Init = init;
     var clickTabConsulta = false;
+    var up_consulta;
+    var btn_consultar;
 
     function init() {
         initControls();
@@ -9,15 +11,20 @@
 
     function initControls() {
 
+        up_consulta = $('#ctl00_body_up_consulta');
+        btn_consultar = $('#ctl00_body_btn_consultar');
+
         $("#tabs").tabs({
             activate: function (event, ui) {
                 if (ui.newPanel.selector === '#tabs-2' && !clickTabConsulta) {
                     $('#ctl00_body_btn_consultar').trigger('click');
-                    clickTabConsulta = true;
                 }
             }
         });
-        $('#ctl00_body_btn_consultar').button();
+
+        $(btn_consultar).button().click(function () {
+            $(btn_consultar).addClass('ui-state-disabled').val('Actualizando registros ...');
+        });
 
         $('#ctl00_body_btn_guardar').button();
 
@@ -27,44 +34,15 @@
 
         $('#ctl00_body_txt_referencia').mask('99-9999-9999999');
 
-        //        $('#ctl00_body_rep_servicios_ctl00_up_servicios').panelReady(function () {
-        //            autoCompletePedido();
-        //        });
-
-        //        $('#ctl00_body_up_trafico').panelReady(function () {
-        //            console.log('up_trafico');
-        //            autoCompletePedido();
-        //        });
-
         $('#ctl00_body_rep_servicios_ctl00_up_servicios').panelReady(function () {
             $('#accordion').accordion('refresh');
         });
+
+        $(up_consulta).panelReady(function () {
+            $(btn_consultar).removeClass('ui-state-disabled').val('Actualizar registros');
+        });
     }
 
-    //    function autoCompletePedido() {
-    //        $(".txtPedidos").autocomplete({
-    //            source: function (request, response) {
-    //                var data = $.parseJSON($('#ctl00_body_hf_pedidos').val());
-
-    //                data = $.grep(data, function (obj, idx) {
-    //                    return obj.label.substring(0, request.term.length) == request.term;
-    //                });
-    //                response(data);
-    //            },
-    //            minLength: 2,
-    //            select: function (event, ui) {
-    //                //console.log("Selected: " + ui.item.value + " aka " + ui.item.Id);
-    //                $('#accordion').accordion('refresh');
-    //                var spanProveedor = $(this).next().children('span').first();
-    //                $(spanProveedor).html('Proveedor: ' + ui.item.Proveedor);
-    //                var spanPiezas = $(spanProveedor).next();
-    //                $(spanPiezas).html('Piezas: ' + ui.item.Piezas);
-    //                var pnl_pedido = $(this).next().next();
-    //                $(pnl_pedido).removeClass('hidden');
-    //                $('#accordion').accordion('refresh');
-    //            }
-    //        });
-    //    }
 }
 
 var master = new webApp.Master;
